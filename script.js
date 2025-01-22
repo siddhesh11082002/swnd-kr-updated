ecb814bc063212bb4cbf1fdb50a140688c65dde6
siddhesh <siddheshkt11@gmail.com>
2025-01-22 12:22:56 +0530

    firebase added

--- script.js
+++ script.js
@@ -255,6 +255,33 @@ representatives.forEach(representative => {
     representativeGrid.appendChild(card);
 });
 
+
+// voice section
+
+   
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+    apiKey: "AIzaSyAz98QmhgFapE7PY7ShVW2fHQcPCvHVkX8",
+    authDomain: "swnd-terminus.firebaseapp.com",
+    projectId: "swnd-terminus",
+    storageBucket: "swnd-terminus.firebasestorage.app",
+    messagingSenderId: "530194613372",
+    appId: "1:530194613372:web:6527065b99481ac709f5f9",
+    measurementId: "G-784NW86FSV"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    
+
     const form = document.getElementById("add-voice-form"); // Form element
     const grid = document.getElementById("voice-grid"); // Voice grid container
     const adminToggle = document.getElementById("admin-toggle"); // Admin toggle button
