a9df8e37ca06f98b14e83e23f5a1ac1b20d1bd9c
siddhesh <siddheshkt11@gmail.com>
2025-01-22 13:19:16 +0530

    some minor bugs in content changed

--- script.js
+++ script.js
@@ -257,8 +257,6 @@ representatives.forEach(representative => {
 
 
 // voice section
-
-   
     // Import the functions you need from the SDKs you need
     import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
     import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
